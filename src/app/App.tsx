1a3db93991d45c8d9ac38245d255789de9b55fca
Kubacki Wojciech <kubackiwo@gmail.com>
2023-05-04 16:28:01 +0200

    added array with prevent from duplicating letters

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,40 @@
-import React, { useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 import './app.sass'
 import Drawing from '../drawing/Drawing'
 import WordInput from '../wordInput/WordInput'
 
+export const guessedLettersContext = createContext<string[]>([])
+
 export default function App() {
   const word = 'the most important';
   const [selectedWord, setSelectedWord] = useState(word);
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', e => {
+      keyInputHandler(guessedLetters, setGuessedLetters, e)
+    })
+  }, [])
 
   return (
-    <div id='app-container'
-      onKeyDown={e => {
-        console.log('dupa')
-        keyInputHandler({guessedLetters, setGuessedLetters, e})
-      }}
-    >
-        <Drawing />
-        <WordInput 
+    <div id='app-container'>
+      <span>{guessedLetters}</span>
+      <Drawing />
+      <WordInput
         word={selectedWord}
         guessedLetters={guessedLetters}
-        />
+      />
     </div>
   )
 }
 
-interface keyInputHandlerParams {
-  guessedLetters: string[],
-  setGuessedLetters: (x: string[]) => void,
-  e: React.KeyboardEvent
-}
-
-function keyInputHandler(param: keyInputHandlerParams) {
-  console.log('param.e.code')
-  console.log(param.e.code)
+function keyInputHandler(guessedLetters: string[], setGuessedLetters: (x: string[]) => void, e: KeyboardEvent) {
+  let updatedGuessedLetters = guessedLetters;
+  if (!updatedGuessedLetters.includes(e.key)) {
+    updatedGuessedLetters.push(e.key);
+    setGuessedLetters(structuredClone(updatedGuessedLetters));
+  } else {
+    console.log('You already have typed this letter')
+  }
 }
